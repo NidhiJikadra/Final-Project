d1aa5349cca58e83b0f3df8cbc29e2a365b1bc50
Nidhi Jikadra <nidhijikadra3103@gmail.com>
2023-08-21 19:47:49 +0530

    Sign up done

--- JS/index.js
+++ JS/index.js
@@ -6,10 +6,26 @@ document.getElementById("signup").addEventListener("submit",(e)=>{
         mobileno : document.getElementById("mno").value,
         password : document.getElementById("password").value,
     }
+    if(user.username.length>0){
+        if(user.mobileno.length==10){
+            if(user.password.length>8){
 
-    fetch("http://localhost:3000/users",{
-        method : "POST",
-        headers : {"Content-type":"application/json"},
-        body: JSON.stringify(user)
-})
+                fetch("http://localhost:3000/users",{
+                    method : "POST",
+                    headers : {"Content-type":"application/json"},
+                    body: JSON.stringify(user) 
+                })
+                window.location.href = 'login.html';
+            }
+            else{
+                alert("Enter Password greater than 8 characters.")
+            }
+        }
+        else{
+            alert("Enter proper mobile number.")
+        }
+    }
+    else{
+        alert("Enter Username.")
+    }
 })
